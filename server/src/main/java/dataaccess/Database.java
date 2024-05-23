f181d68cec04c1498ed161dc0c44d5d13888dff0
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-22 19:43:34 -0600

    Added databaserelated getUsers

--- server/src/main/java/dataaccess/Database.java
+++ server/src/main/java/dataaccess/Database.java
@@ -1,4 +1,30 @@
 package dataaccess;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+
+
 public class Database {
+    private static Database database;
+    private Map<String, UserData> users;
+    private Map<Integer, GameData> games;
+    private Map<String, AuthData> authTokens;
+
+    private Database() {
+        this.users = new HashMap<>();
+        this.games = new HashMap<>();
+        this.authTokens = new HashMap<>();
+    }
+
+    public Map<String, UserData> getUsers() {
+        return users;
+    }
+
+
+
 }
