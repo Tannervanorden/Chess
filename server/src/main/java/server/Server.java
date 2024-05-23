89222e4a0441ac7dd733780f65d26d05c9ac0d8a
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-23 12:22:38 -0600

    I am attemptin to add a spark endpoint for the register handler

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,6 +11,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", ((request, response) -> (new RegisterHandler).register(request, response)));
 
         Spark.awaitInitialization();
         return Spark.port();
