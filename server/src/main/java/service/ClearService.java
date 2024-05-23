f76d29328ad02a6357ec6613634704bcca33acc9
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-22 19:38:24 -0600

    added the Database class where I think it should be

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,4 +1,12 @@
 package service;
 
+import dataaccess.Database;
+import dataaccess.DataAccessException;
+
 public class ClearService {
+    public void clear() throws DataAccessException {
+        Database db = new Database();
+        db.clear()
+    }
+
 }
