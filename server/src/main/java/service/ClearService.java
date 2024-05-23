9503e27220ff82e2f744353afd69c1cd5d6906fa
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-23 09:11:22 -0600

    Added clear

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -1,7 +1,20 @@
 package service;
 
 import dataaccess.DataAccessException;
+import dataaccess.AuthDAO;
+import dataaccess.UserDAO;
+import dataaccess.GameDAO;
+
 
 public class ClearService {
+    private GameDAO gameDAO;
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+
+    public ClearService() {
+        gameDAO.clear();
+        userDAO.clear();
+        authDAO.clear();
+    }
 
 }
