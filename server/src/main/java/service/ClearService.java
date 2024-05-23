b21c54506a7f3df6ff5dcb4759e798421fa4bcfb
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-23 10:17:45 -0600

    Changed to seven service classes

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -7,14 +7,8 @@ import dataaccess.GameDAO;
 
 
 public class ClearService {
-    private GameDAO gameDAO;
-    private UserDAO userDAO;
-    private AuthDAO authDAO;
 
     public ClearService(GameDAO gameDAO, UserDAO userDAO, AuthDAO authDAO) {
-        this.gameDAO = gameDAO;
-        this.userDAO = userDAO;
-        this.authDAO = authDAO;
         gameDAO.clear();
         userDAO.clear();
         authDAO.clear();
