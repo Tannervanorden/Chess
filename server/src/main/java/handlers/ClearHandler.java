3826d7e9a2121ca237c90cf2af14298c764589a6
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-24 13:08:02 -0600

    added first implementation of clear

--- server/src/main/java/handlers/ClearHandler.java
+++ server/src/main/java/handlers/ClearHandler.java
@@ -2,15 +2,24 @@ package handlers;
 
 import com.google.gson.Gson;
 import dataaccess.DataAccessException;
-import model.AuthData;
-import model.UserData;
 import service.ClearService;
 import spark.Request;
 import spark.Response;
+
+import java.util.Map;
 
 public class ClearHandler {
     private Gson gson = new Gson();
     private ClearService clearService = new ClearService();
 
-
+    public Object clear(Request request, Response response) {
+        try {
+            clearService.clear();
+            response.status(200);
+            return gson.toJson(new Object());
+        } catch (DataAccessException e) {
+            response.status(500);
+            return gson.toJson(Map.of("message", "Error: (description of error)"));
+        }
+    }
 }
--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -4,17 +4,19 @@ import dataaccess.DataAccessException;
 import dataaccess.AuthDAO;
 import dataaccess.UserDAO;
 import dataaccess.GameDAO;
-import model.AuthData;
-import model.UserData;
 
 
 public class ClearService extends GenericService {
 
-    public ClearService() {
         public void clear() throws DataAccessException {
 
             UserDAO userDAO = GenericService.getUserDAO();
             AuthDAO authDAO = GenericService.getAuthDAO();
             GameDAO gameDAO = GenericService.getGameDAO();
+
+            userDAO.clear();
+            authDAO.clear();
+            gameDAO.clear();
+
     }
 }
