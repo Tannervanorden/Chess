c657522f2b3535ab652802546d4a22a4de23108c
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-26 08:32:46 -0600

    started adding listGame

--- server/src/main/java/service/ListGamesService.java
+++ server/src/main/java/service/ListGamesService.java
@@ -1,4 +1,18 @@
 package service;
 
+import com.google.gson.Gson;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import service.ListGamesService;
+
 public class ListGamesService {
+    private Gson gson = new Gson();;
+    private ListGamesService service = new ListGamesService();
+
+    public Object listGames(Request request, Response response) {
+        try {
+            String authToken = request.headers("authorization");
+        }
+    }
 }
