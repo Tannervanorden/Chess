f2a802b360395ef088726e38e15991c0948da3ce
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-26 09:15:29 -0600

    passed one listGames test

--- server/src/main/java/service/ListGamesService.java
+++ server/src/main/java/service/ListGamesService.java
@@ -1,13 +1,43 @@
 package service;
 
-import com.google.gson.Gson;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
 import model.GameData;
-import spark.Request;
-import spark.Response;
-import spark.Route;
-import service.ListGamesService;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class ListGamesService {
+public class ListGamesService extends GenericService {
+    private GameDAO gameDAO = GenericService.getGameDAO();
+    private AuthDAO authDAO = GenericService.getAuthDAO();
+
+    public Map<String, Object> listGames(String authToken) throws Exception {
+        if (!authDAO.validateToken(authToken)) {
+            throw new Exception("Unauthorized");
+        }
+
+        try {
+            Map<Integer, GameData> games = gameDAO.getGames();
+            List<Map<String, Object>> gamesList = new ArrayList<>();
+
+            //loop through each game in the gameDAO and add to an array list
+            for (GameData game : games.values()) {
+                Map<String, Object> gameData = new HashMap<>();
+                gameData.put("gameID", game.gameID());
+                gameData.put("gameName", game.gameName());
+                gameData.put("whiteUsername", game.whiteUsername());
+                gameData.put("blackUsername", game.blackUsername());
+                gamesList.add(gameData);
+            }
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("games", gamesList);
+            return response;
+        } catch (Exception e) {
+            throw new Exception("Failed to get game list");
+        }
+    }
+
 }
