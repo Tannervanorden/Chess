b63c5aec698856f6449a9aa7cb431b57d76705f4
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-26 08:43:54 -0600

    accidentally working in service class instead of handler class

--- server/src/main/java/handlers/ListGameHandler.java
+++ server/src/main/java/handlers/ListGameHandler.java
@@ -1,4 +1,31 @@
 package handlers;
 
+import com.google.gson.Gson;
+import service.ListGamesService;
+import spark.Request;
+import spark.Response;
+
+import java.util.Map;
+
 public class ListGameHandler {
+    private Gson gson = new Gson();;
+    private ListGamesService listGamesService = new ListGamesService();
+
+    public Object listGames(Request request, Response response) {
+        try {
+            String authToken = request.headers("authorization");
+
+            if (authToken == null) {
+                response.status(401);
+                return gson.toJson(Map.of("Message", "Error, bad authToken"));
+            }
+
+            Map<String, Object> games = listGamesService.listGames(authToken);
+            response.status(200);
+            return gson.toJson(games);
+        } catch (Exception e) {
+            response.status(500);
+            return gson.toJson(Map.of("Message", e.getMessage()));
+        }
+    }
 }
--- server/src/main/java/service/ListGamesService.java
+++ server/src/main/java/service/ListGamesService.java
@@ -10,21 +10,4 @@ import service.ListGamesService;
 import java.util.Map;
 
 public class ListGamesService {
-    private Gson gson = new Gson();;
-    private ListGamesService listGamesService = new ListGamesService();
-
-    public Object listGames(Request request, Response response) {
-        try {
-            String authToken = request.headers("authorization");
-
-            if (authToken == null) {
-                response.status(401);
-                return gson.toJson(Map.of("Message", "Error, bad authToken"));
-            }
-
-            List<GameData> games = listGamesService.listGames(authToken);
-            response.status(200);
-            return gson.toJson(games);
-        }
-    }
 }
