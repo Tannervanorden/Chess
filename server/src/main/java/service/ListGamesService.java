30aecce8b13755a94f93d122f78e961ca2019fa6
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-26 08:41:35 -0600

    Struggling betwen list and map, may switch to map

--- server/src/main/java/service/ListGamesService.java
+++ server/src/main/java/service/ListGamesService.java
@@ -1,18 +1,30 @@
 package service;
 
 import com.google.gson.Gson;
+import model.GameData;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 import service.ListGamesService;
 
+import java.util.Map;
+
 public class ListGamesService {
     private Gson gson = new Gson();;
-    private ListGamesService service = new ListGamesService();
+    private ListGamesService listGamesService = new ListGamesService();
 
     public Object listGames(Request request, Response response) {
         try {
             String authToken = request.headers("authorization");
+
+            if (authToken == null) {
+                response.status(401);
+                return gson.toJson(Map.of("Message", "Error, bad authToken"));
+            }
+
+            List<GameData> games = listGamesService.listGames(authToken);
+            response.status(200);
+            return gson.toJson(games);
         }
     }
 }
