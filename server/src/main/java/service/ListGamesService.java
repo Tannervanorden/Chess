429fa7b0a3df32447a6ecc5a1dce07f3e9e5acdb
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-26 08:55:42 -0600

    First implementation of listGames failed all tests

--- server/src/main/java/service/ListGamesService.java
+++ server/src/main/java/service/ListGamesService.java
@@ -1,13 +1,32 @@
 package service;
 
-import com.google.gson.Gson;
-import model.GameData;
-import spark.Request;
-import spark.Response;
-import spark.Route;
-import service.ListGamesService;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class ListGamesService {
+public class ListGamesService extends GenericService {
+    private GameDAO gameDAO = GenericService.getGameDAO();
+    private AuthDAO authDAO = GenericService.getAuthDAO();
+
+    public Map<String, Object> listGames(String authToken) throws Exception {
+        if (!authDAO.validateToken(authToken)) {
+            throw new Exception("Unauthorized");
+        }
+
+        try {
+            Map<Integer, GameData> games = gameDAO.getGames();
+            Map<String, Object> response = new HashMap<>();
+            response.put("games", games);
+            return response;
+        } catch (Exception e) {
+            throw new Exception("Failed to get games");
+        }
+    }
+
 }
