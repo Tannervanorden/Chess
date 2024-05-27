3f13fc45e927d1a2e78bfb4997e0636d4d131a0b
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-27 08:36:08 -0600

    added packages and started JoinGameHandler

--- server/src/main/java/handlers/JoinGameHandler.java
+++ server/src/main/java/handlers/JoinGameHandler.java
@@ -1,4 +1,11 @@
 package handlers;
 
+import com.google.gson.Gson;
+import service.JoinGameService;
+import spark.Request;
+import spark.Response;
+
 public class JoinGameHandler {
+    private Gson gson = new Gson();
+
 }
