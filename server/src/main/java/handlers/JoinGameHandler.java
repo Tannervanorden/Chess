dd2f3746f1312410ade67f4b0abc26faa0f9f9f9
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-27 08:43:12 -0600

    added basic implementation of handler. starting service class

--- server/src/main/java/handlers/JoinGameHandler.java
+++ server/src/main/java/handlers/JoinGameHandler.java
@@ -1,11 +1,31 @@
 package handlers;
 
 import com.google.gson.Gson;
+import model.GameData;
 import service.JoinGameService;
 import spark.Request;
 import spark.Response;
 
+import java.util.Map;
+
 public class JoinGameHandler {
     private Gson gson = new Gson();
+    private JoinGameService joinGameService = new JoinGameService();
+
+     public Object joinGame(Request request, Response response) {
+         try {
+             String authToken = request.headers("authorization");
+             GameData requestBody = gson.fromJson(request.body(), GameData.class);
+
+             if (authToken == null || authToken.isEmpty()) {
+                 response.status(401);
+                 return gson.toJson(Map.of("message", "Error: unauthorized"));
+             }
+
+             int id = requestBody.gameID();
+             GameData game = joinGameService.joinGame(id, authToken);
+
+         }
+     }
 
 }
