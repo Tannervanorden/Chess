37fe7bfb5139ef80add9bfbfdc8daf6dd3e2ada3
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-28 10:21:49 -0600

    Added tests

--- server/src/test/java/passoff/SelfTests/ServiceTests.java
+++
@@ -1,4 +0,0 @@
-package passoff.SelfTests;
-
-public class ServiceTests {
-}
---
+++ server/src/test/java/passoff/service/ServiceTests.java
@@ -0,0 +1,60 @@
+package passoff.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServiceTest {
+
+    @Test
+    void clearPositive() {
+    }
+
+    @Test
+    void createGamePositive() {
+    }
+
+    @Test
+    void createGameNegative() {
+    }
+
+    @Test
+    void joinGamePositive() {
+    }
+
+    @Test
+    void joinGameNegative() {
+    }
+
+    @Test
+    void listGamesPositive() {
+    }
+
+    @Test
+    void listGamesNegative() {
+    }
+
+    @Test
+    void loginPositive() {
+    }
+
+    @Test
+    void loginNegative() {
+    }
+
+    @Test
+    void logoutPositive() {
+    }
+
+    @Test
+    void logoutNegative() {
+    }
+
+    @Test
+    void registerPositive() {
+    }
+
+    @Test
+    void registerNegative() {
+    }
+}
