5c5052f016536c40010d9eaee682346ee8363489
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-05-31 14:58:33 -0600

    added creation of MySQLUser

--- server/src/main/java/dataaccess/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/MySQLUserDAO.java
@@ -1,4 +1,32 @@
 package dataaccess;
 
+import java.sql.SQLException;
+
 public class MySQLUserDAO {
+    private static String TABLE_NAME = "user";
+
+    public MySQLUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    private final String[] createStatements = {
+            "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
+                    " username VARCHAR(200) PRIMARY KEY," +
+                    " password VARCHAR(200) NOT NULL," +
+                    " email VARCHAR(200) NOT NULL" +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci"
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to configure database: " + ex.getMessage());
+        }
+    }
 }
