1348b5642ee1c50c4ec7c27fef5ceafe90ea9a95
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-06-03 07:05:03 -0600

    Fixing my addGame made me pass all of my tests from phase 3

--- server/src/main/java/dataaccess/MySQLGameDAO.java
+++ server/src/main/java/dataaccess/MySQLGameDAO.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MySQLGameDAO {
     private static String TABLE_NAME = "game";
@@ -19,7 +21,7 @@ public class MySQLGameDAO {
 
     private final String[] createStatements = {
             "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
-                    "  `gameID` INT AUTO_INCREMENT PRIMARY KEY," +
+                    "  `gameID` INT PRIMARY KEY," +
                     "  `whiteUsername` VARCHAR(100) NULL," +
                     "  `blackUsername` VARCHAR(100) NULL," +
                     "  `gameName` VARCHAR(100) NULL," +
@@ -43,16 +45,18 @@ public class MySQLGameDAO {
     public GameData getGame(int gameID) throws DataAccessException {
         String query = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM " + TABLE_NAME + " WHERE gameID = ?";
         try (Connection conn = DatabaseManager.getConnection();
-             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-            preparedStatement.setInt(1, gameID);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+             PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setInt(1, gameID);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                //Check if there even is a result after querying
                 if (resultSet.next()) {
                     int id = resultSet.getInt("gameID");
                     String whiteUsername = resultSet.getString("whiteUsername");
                     String blackUsername = resultSet.getString("blackUsername");
                     String gameName = resultSet.getString("gameName");
                     String gameJson = resultSet.getString("game");
-                    ChessGame game = gson.fromJson(gameJson, ChessGame.class);  // Deserializing the game
+                    // Deserialize gson to a ChessGame class
+                    ChessGame game = gson.fromJson(gameJson, ChessGame.class);
                     return new GameData(id, whiteUsername, blackUsername, gameName, game);
                 } else {
                     return null;
@@ -66,14 +70,14 @@ public class MySQLGameDAO {
     public void updateGame(int id, GameData game) throws DataAccessException {
         String query = "UPDATE " + TABLE_NAME + " SET whiteUsername = ?, blackUsername = ?, gameName = ?, game = ? WHERE gameID = ?";
         try (Connection conn = DatabaseManager.getConnection();
-             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-            preparedStatement.setString(1, game.whiteUsername());
-            preparedStatement.setString(2, game.blackUsername());
-            preparedStatement.setString(3, game.gameName());
+             PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setString(1, game.whiteUsername());
+            statement.setString(2, game.blackUsername());
+            statement.setString(3, game.gameName());
             String gameJson = gson.toJson(game.game());
-            preparedStatement.setString(4, gameJson);
-            preparedStatement.setInt(5, id);
-            preparedStatement.executeUpdate();
+            statement.setString(4, gameJson);
+            statement.setInt(5, id);
+            statement.executeUpdate();
         } catch (SQLException ex) {
             throw new DataAccessException("Error updating game: " + ex.getMessage());
         }
@@ -93,15 +97,44 @@ public class MySQLGameDAO {
     }
 
     public void addGame(GameData game) throws DataAccessException {
-        String query = "INSERT INTO " + TABLE_NAME + " (whiteUsername, blackUsername, gameName) VALUES (?, ?, ?)";
+        String query = "INSERT INTO " + TABLE_NAME + " (gameID, whiteUsername, blackUsername, gameName, game) VALUES (?, ?, ?, ?, ?)";
         try (Connection conn = DatabaseManager.getConnection();
-             PreparedStatement preparedStatement = conn.prepareStatement(query)) {
-            preparedStatement.setString(1, game.whiteUsername());
-            preparedStatement.setString(2, game.blackUsername());
-            preparedStatement.setString(3, game.gameName());
-            preparedStatement.executeUpdate();
+             PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setInt(1, game.gameID());
+            statement.setString(2, game.whiteUsername());
+            statement.setString(3, game.blackUsername());
+            statement.setString(4, game.gameName());
+            String gameJson = gson.toJson(game.game());
+            statement.setString(5, gameJson);
+            statement.executeUpdate();
         } catch (SQLException ex) {
             throw new DataAccessException("Error adding game: " + ex.getMessage());
         }
     }
+
+    public Map<Integer, GameData> getGames() throws DataAccessException {
+        String query = "SELECT gameID, whiteUsername, blackUsername, gameName, game FROM " + TABLE_NAME;
+        Map<Integer, GameData> games = new HashMap<>();
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement statement = conn.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery()) {
+            // Get all Games
+            while (resultSet.next()) {
+                int id = resultSet.getInt("gameID");
+                String whiteUsername = resultSet.getString("whiteUsername");
+                String blackUsername = resultSet.getString("blackUsername");
+                String gameName = resultSet.getString("gameName");
+                String gameJson = resultSet.getString("game");
+                // Deserialize gson to a ChessGame class
+                ChessGame game = gson.fromJson(gameJson, ChessGame.class);
+                GameData gameData = new GameData(id, whiteUsername, blackUsername, gameName, game);
+                games.put(id, gameData);
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to get games: " + ex.getMessage());
+        }
+        return games;
+    }
+
+
 }
--- server/src/main/java/handlers/ListGameHandler.java
+++ server/src/main/java/handlers/ListGameHandler.java
@@ -1,31 +1,31 @@
-//package handlers;
-//
-//import com.google.gson.Gson;
-//import service.ListGamesService;
-//import spark.Request;
-//import spark.Response;
-//
-//import java.util.Map;
-//
-//public class ListGameHandler {
-//    private Gson gson = new Gson();;
-//    private ListGamesService listGamesService = new ListGamesService();
-//
-//    public Object listGames(Request request, Response response) {
-//        try {
-//            String authToken = request.headers("authorization");
-//
-//            Map<String, Object> games = listGamesService.listGames(authToken);
-//            response.status(200);
-//            return gson.toJson(games);
-//        } catch (Exception e) {
-//            String errorMessage = e.getMessage();
-//            if (errorMessage.equals("Unauthorized")) {
-//                response.status(401);
-//                return gson.toJson(Map.of("message", "Error: Unauthorized"));
-//            }
-//            response.status(500);
-//            return gson.toJson(Map.of("message", e.getMessage()));
-//        }
-//    }
-//}
+package handlers;
+
+import com.google.gson.Gson;
+import service.ListGamesService;
+import spark.Request;
+import spark.Response;
+
+import java.util.Map;
+
+public class ListGameHandler {
+    private Gson gson = new Gson();;
+    private ListGamesService listGamesService = new ListGamesService();
+
+    public Object listGames(Request request, Response response) {
+        try {
+            String authToken = request.headers("authorization");
+
+            Map<String, Object> games = listGamesService.listGames(authToken);
+            response.status(200);
+            return gson.toJson(games);
+        } catch (Exception e) {
+            String errorMessage = e.getMessage();
+            if (errorMessage.equals("Unauthorized")) {
+                response.status(401);
+                return gson.toJson(Map.of("message", "Error: Unauthorized"));
+            }
+            response.status(500);
+            return gson.toJson(Map.of("message", e.getMessage()));
+        }
+    }
+}
