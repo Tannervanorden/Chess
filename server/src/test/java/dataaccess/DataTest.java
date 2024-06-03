361c02640c6856700cdaa95db942616900105887
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-06-03 07:56:36 -0600

    setup tests and got two done

---
+++ server/src/test/java/dataaccess/DataTest.java
@@ -0,0 +1,59 @@
+package dataaccess;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.*;
+import passoff.model.*;
+import passoff.server.TestServerFacade;
+import server.Server;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class DataTest {
+    private MySQLAuthDAO authDAO;
+    private MySQLGameDAO gameDAO;
+    private MySQLUserDAO userDAO;
+
+
+    @BeforeEach
+    public void setUp() {
+        try {
+            authDAO = new MySQLAuthDAO();
+            authDAO.clear();
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+        try {
+            userDAO = new MySQLUserDAO();
+            userDAO.clear();
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+        try {
+            gameDAO = new MySQLGameDAO();
+            gameDAO.clear();
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @Order(1)
+    public void testAddAuth() throws DataAccessException {
+        AuthData authData = new AuthData("token1", "user1");
+        authDAO.addAuth("token1", authData);
+        assertEquals(authData, authDAO.getAuth().get("token1"));
+    }
+
+    @Test
+    @Order(2)
+    public void testAddGame() throws DataAccessException {
+        GameData game = new GameData(1, "whiteUsername", "blackUsermane", "test", null);
+        gameDAO.addGame(game);
+        GameData checkGame = gameDAO.getGame(1);
+        assertEquals(game, checkGame);
+    }
+
+
+
+}
