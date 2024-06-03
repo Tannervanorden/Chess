caba611be8d4c21e3dcf4f2dc7814aa34f562501
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-06-03 08:02:43 -0600

    Added 3 more tests

--- server/src/test/java/dataaccess/DataTest.java
+++ server/src/test/java/dataaccess/DataTest.java
@@ -6,7 +6,7 @@ import passoff.model.*;
 import passoff.server.TestServerFacade;
 import server.Server;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DataTest {
@@ -39,7 +39,7 @@ public class DataTest {
 
     @Test
     @Order(1)
-    public void testAddAuth() throws DataAccessException {
+    public void testAddAuthPositive() throws DataAccessException {
         AuthData authData = new AuthData("token1", "user1");
         authDAO.addAuth("token1", authData);
         assertEquals(authData, authDAO.getAuth().get("token1"));
@@ -47,6 +47,30 @@ public class DataTest {
 
     @Test
     @Order(2)
+    public void testAddAuthNegative() throws DataAccessException {
+        AuthData authData = new AuthData("token1", "user1");
+        authDAO.addAuth("token1", authData);
+        assertNull(authDAO.getAuth().get("token2"));
+    }
+
+
+    @Test
+    @Order(3)
+    public void testValidateAuthPositive() throws DataAccessException {
+        AuthData authData = new AuthData("token1", "user1");
+        authDAO.addAuth("token1", authData);
+        assertTrue(authDAO.validateToken("token1"));
+    }
+    @Test
+    @Order(4)
+    public void testValidateAuthNegative() throws DataAccessException {
+        AuthData authData = new AuthData("token1", "user1");
+        authDAO.addAuth("token1", authData);
+        assertFalse(authDAO.validateToken("token2"));
+    }
+
+    @Test
+    @Order(5)
     public void testAddGame() throws DataAccessException {
         GameData game = new GameData(1, "whiteUsername", "blackUsermane", "test", null);
         gameDAO.addGame(game);
