842e817863b5bf9bea16e1bc3dbf0bd4d3e07b2f
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-06-06 15:25:34 -0600

    trying to figure out the whole post thing

--- client/src/main/java/sf/ServerFacade.java
+++ client/src/main/java/sf/ServerFacade.java
@@ -1,10 +1,19 @@
 package sf;
 
 import com.google.gson.Gson;
+import model.AuthData;
+import model.UserData;
 
 public class ServerFacade {
     private String serverUrl = "http://localhost:8080";
     private Gson gson = new Gson();
+
+    //Login
+    public AuthData login(String username, String password) throws Exception {
+        String endpoint = "/login";
+        UserData user = new UserData(username, password, null);
+        return sendPostRequest(endpoint, user, AuthData.class);
+    }
 
 
 }
