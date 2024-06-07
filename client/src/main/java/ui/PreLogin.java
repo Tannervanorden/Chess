ea9e60b2aa3b1e54c43b5e1217f62008f6631126
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-06-07 16:34:31 -0600

    Started working on menu

--- client/src/main/java/ui/PreLogin.java
+++ client/src/main/java/ui/PreLogin.java
@@ -1,4 +1,16 @@
 package ui;
 
+import java.util.Scanner;
+
 public class PreLogin {
+    private Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        displayPreLoginUI();
+    }
+
+    private static void displayPreLoginUI() {
+        System.out.println("Welcome to Chess!");
+    }
+
 }
