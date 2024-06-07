fdcd4ff4ec22b715c91252078b566976de6ea56e
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-06-07 13:58:38 -0600

    got something added but it is stretching the board

--- client/src/main/java/ui/Board.java
+++ client/src/main/java/ui/Board.java
@@ -1,27 +1,49 @@
 package ui;
 
 import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessPosition;
 
 
 public class Board {
 
-    private static final int BOARD_SIZE = 8;
+    private ChessGame game;
 
-    public static void main(String[] args) {
-        drawChessBoard();
+    private static final int boardSize = 8;
+
+    public Board(ChessGame game) {
+        this.game = game;
     }
 
-    private static void drawChessBoard() {
+    public static void main(String[] args) {
+        ChessGame game = new ChessGame();
+
+        Board board = new Board(game);
+
+        board.drawChessBoard();
+    }
+
+    private void drawChessBoard() {
+        ChessBoard board = game.getBoard();
         System.out.print("   a  b  c  d  e  f  g  h\n");
 
-        for (int row = 0; row < BOARD_SIZE; row++) {
+        for (int row = 0; row < boardSize; row++) {
             System.out.print(8 - row + " ");
 
-            for (int col = 0; col < BOARD_SIZE; col++) {
+            for (int col = 0; col < boardSize; col++) {
                 // Work on squares
                 String color = (row + col) % 2 == 0 ? EscapeSequences.SET_BG_COLOR_WHITE : EscapeSequences.SET_BG_COLOR_LIGHT_GREY;
                 System.out.print(color);
-                System.out.print(EscapeSequences.EMPTY);
+
+                ChessPosition position = new ChessPosition(row + 1, col + 1);
+                if (board.getPiece(position) != null){
+                    System.out.print(EscapeSequences.WHITE_KING);
+                    System.out.print(EscapeSequences.EMPTY);
+                }
+                else {
+                    System.out.print(EscapeSequences.EMPTY);
+                }
+
             }
             System.out.println(EscapeSequences.RESET_BG_COLOR);
         }
