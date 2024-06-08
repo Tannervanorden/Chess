a9a17ccd62c9e111807d9bab4eb77fa584ad891f
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-06-08 12:43:57 -0600

    Starting postlogin

--- client/src/main/java/ui/PostLogin.java
+++ client/src/main/java/ui/PostLogin.java
@@ -1,4 +1,18 @@
 package ui;
 
+import sf.ServerFacade;
+
+import java.util.Scanner;
+
 public class PostLogin {
+    private Scanner scanner = new Scanner(System.in);
+    private ServerFacade serverFacade = new ServerFacade();
+
+    public static void main(String[] args) {
+        PostLogin postLogin = new PostLogin();
+        PostLogin.displayPostLoginUI();
+    }
+
+    private static void displayPostLoginUI() {
+    }
 }
