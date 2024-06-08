d3ce8b9792814c2b1bf1ebb2a144d76fc4aaab5c
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-06-08 14:55:54 -0600

    Need to get authToken

--- client/src/main/java/ui/PostLogin.java
+++ client/src/main/java/ui/PostLogin.java
@@ -13,10 +13,32 @@ public class PostLogin {
         PostLogin.displayPostLoginUI();
     }
 
-    private static void displayPostLoginUI() {
+    public void displayPostLoginUI() {
 
         while (true) {
-            System.out.print("Logged in");
+            System.out.print("Logged in\n");
+            System.out.print("Select an option: \n");
+            System.out.print("1. Register\n");
+            System.out.print("2. Login\n");
+            System.out.print("3. Quit\n");
+            System.out.print("4. Help\n");
+
+            System.out.print("Enter: ");
+
+            int choice = scanner.nextInt();
+
+            if (choice == 1){
+
+                System.out.print("Enter a Name:");
+                String name = scanner.next();
+
+                try {
+                    serverFacade.createGame(name, authToken);
+                    System.out.println("Game Created Successfully!");
+                } catch (Exception e) {
+                    System.out.println("Game Creation failed: " + e.getMessage());
+                }
+            }
 
 
         }
