d252aedc01aef9ec699a6c7ef5461e792e6e88d1
Tannervanorden <126115670+Tannervanorden@users.noreply.github.com>
2024-06-08 16:22:34 -0600

    Starting delete, Still figuring out what I need to do to make delete different that post

--- client/src/main/java/sf/ServerFacade.java
+++ client/src/main/java/sf/ServerFacade.java
@@ -101,6 +101,62 @@ public class ServerFacade {
             }
         }
     }
+
+
+    public <T> T doDelete(String endpoint, Object requestBody, Class<T> responseClass, String authToken) throws Exception {
+        URL url = new URL(urlString + endpoint);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setReadTimeout(5000);
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+
+        // Set HTTP request headers, if necessary
+        // connection.addRequestProperty("Accept", "text/html");
+        if (authToken != null) {
+            connection.setRequestProperty("Authorization", authToken);
+        }
+
+        connection.connect();
+
+        try (OutputStream requestBodyStream = connection.getOutputStream();) {
+            String jsonRequest = gson.toJson(requestBody);
+            requestBodyStream.write(jsonRequest.getBytes());
+            requestBodyStream.flush();
+        }
+
+        if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+            try (InputStream responseStream = connection.getInputStream()) {
+                InputStreamReader responseStreamReader = new InputStreamReader(responseStream);
+                BufferedReader bufferStream = new BufferedReader(responseStreamReader);
+
+                StringBuilder responseBuilder = new StringBuilder();
+                String inputLine;
+                while ((inputLine = bufferStream.readLine()) != null) {
+                    responseBuilder.append(inputLine);
+                }
+                return gson.fromJson(responseBuilder.toString(), responseClass);
+            }
+        } else {
+            // SERVER RETURNED AN HTTP ERROR
+            try (InputStream responseBodyStream = connection.getErrorStream();
+                 InputStreamReader inputStreamReader = new InputStreamReader(responseBodyStream);
+                 BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
+                StringBuilder responseBody = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    responseBody.append(line);
+                }
+
+                throw new Exception(responseBody.toString());
+            }
+        }
+    }
+
+
     public <T> T doGet(String endpoint, Type responseType, String authToken) throws Exception {
         URL url = new URL(urlString + endpoint);
 
